be447ca65a57c4eb4a020b972a003e6482dbb77b
Anush Venkatakrishnan <54374648+anushkrishnav@users.noreply.github.com>
2024-04-22 21:54:05 +0000

    rename functions

--- src/Vehicle.py
+++ src/Vehicle.py
@@ -5,40 +5,39 @@ class Vehicle:
         self.is_operational = False
         self.speed = 0
 
-    def start_engine(self):
+    def turn_on(self):
         if not self.is_operational:
             self.is_operational = True
             self.speed = 0
-            print(f"The engine of {self.make} {self.model} has started.")
+            print(f"The engine of {self.make} {self.model} has been turned on.")
         else:
-            print(f"{self.make} {self.model} is already running.")
+            print(f"{self.make} {self.model} is already on.")
 
-    def stop_engine(self):
+    def turn_off(self):
         if self.is_operational and self.speed == 0:
             self.is_operational = False
             print(f"{self.make} {self.model} has been turned off.")
         elif self.speed > 0:
             print(f"Reduce speed to 0 before turning off the engine.")
         else:
-            print(f"{self.make} {self.model} is already turned off.")
+            print(f"{self.make} {self.model} is already off.")
 
-    def increase_speed(self, increment):
+    def accelerate(self, increment):
         if self.is_operational:
             self.speed += increment
-            print(f"Speeding up. The speed of {self.make} {self.model} is now {self.speed} km/h.")
+            print(f"Accelerating. The speed of {self.make} {self.model} is now {self.speed} km/h.")
         else:
-            print("Start the engine before speeding up.")
+            print("Start the engine before accelerating.")
 
-    def decrease_speed(self, decrement):
+    def decelerate(self, decrement):
         if self.is_operational and self.speed >= decrement:
             self.speed -= decrement
-            print(f"Slowing down. The speed of {self.make} {self.model} is now {self.speed} km/h.")
+            print(f"Decelerating. The speed of {self.make} {self.model} is now {self.speed} km/h.")
         else:
-            print("Insufficient speed to slow down or the engine is not running.")
+            print("Insufficient speed to decelerate or the engine is not on.")
 
-    def display_status(self):
+    def show_status(self):
         if self.is_operational:
             print(f"{self.make} {self.model} is operational. Current speed: {self.speed} km/h.")
         else:
             print(f"{self.make} {self.model} is not operational.")
-
